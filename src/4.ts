596087d1cee4472ee67fe75ce52a3a233a82baf1
Aleksandr Borysenko <borysenko343@gmail.com>
2023-10-12 18:24:29 +0300

    HW

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,36 @@
+class Key {}
+
+class MyHouse {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  openDoor(enteredKey: Key) {
+    if (enteredKey === this.key) {
+      console.log("Двері відкрито");
+    } else {
+      console.log("Вийди звідси розбійник!");
+    }
+  }
+
+  comeIn(person: Person) {
+    console.log("Заходьте дівчата, то є моя хата...");
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey() {
+    return this.key;
+  }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +39,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
